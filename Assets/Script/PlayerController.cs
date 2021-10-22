91c3588f9e473291a41b94a28fd3e59f4fe4eaf3
SupperH <532334725@qq.com>
2021-10-22 16:21:55 +0800

    单项平台，角色攀爬功能

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     //获取角色的box碰撞体积
     private BoxCollider2D myfeet;
 
+
     //判断是否是地面
     private bool isGround;
 
@@ -25,6 +26,31 @@ public class PlayerController : MonoBehaviour
 
     //判断是否二段跳
     private bool canDoubleJump;
+
+
+    //是否在单项平台上
+    private bool isOneWayPlatform;
+
+    //重置角色层时间
+    public float resetTime;
+
+    //是否为梯子
+    private bool isLadder;
+    //是否正在爬梯子
+    private bool isClimbing;
+    //是否在跳跃
+    private bool isJumping;
+    //是否在二段跳
+    private bool isFalling;
+    //是否在下落
+    private bool isDoubleJumping;
+    //是否在二段下落
+    private bool isDoubleFalling;
+    //爬梯子的速度
+    public float climbSpeed;
+    //默认重力
+    private float playerGravity;
+
     //游戏一开始就会执行的方法
     void Start()
     {
@@ -32,6 +58,8 @@ public class PlayerController : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         myfeet = GetComponent<BoxCollider2D>();
+        //初始重力
+        playerGravity = rigidbody2D.gravityScale;
     }
 
     // 游戏运行时每一帧都会执行的方法，移动放在里面
@@ -40,12 +68,16 @@ public class PlayerController : MonoBehaviour
         //只有玩家存活，才能执行以下动作
         if (GameController.isGameAlive)
         {
+            CheckAirStatus();
             Flip();
             Run();
             Jump();
             CheckGrounded();
             SwitchAnimation();
             //Attack();
+            OneWayPlatfromCheck();
+            CheckLadder();
+            Climb();
         }
 
     }
@@ -77,7 +109,8 @@ public class PlayerController : MonoBehaviour
     }
 
     //移动
-    void Run(){
+    void Run()
+    {
         // 在unity里面的edit的Project settings里的input manager选项打开axis可以看到Horizontal
         //实际上这段代码就是获取配置 
         //-1代表向左，1代表向右，0代表不动 默认是a=-1 d=1
@@ -85,7 +118,7 @@ public class PlayerController : MonoBehaviour
 
         //给玩家设定速度，使用Vector2这个类和vector3的区别就是一个是2d一个是3d，用3也可以只要把z轴速度设置为0就行
         //x轴速度就是移动方向*移动速度，y轴方向就取默认的原速度就行
-        Vector2 playerVel = new Vector2(moveDir*runSpeed, rigidbody2D.velocity.y);
+        Vector2 playerVel = new Vector2(moveDir * runSpeed, rigidbody2D.velocity.y);
         //把设定好的速度赋给刚体变量，让角色移动
         rigidbody2D.velocity = playerVel;
 
@@ -93,7 +126,7 @@ public class PlayerController : MonoBehaviour
         //这里用大于Mathf.Epsilon而不是大于0 Mathf.Epsilon是一个非常非常小的值
         bool playerHasXAxisSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
         //给我们在unity中动画切换的条件参数赋值，第一个参数就是我们在Animator窗口设置的参数名，第二个就传需要的值，bool是setbool其他参数的set方法名大同小异
-        animator.SetBool("Run",playerHasXAxisSpeed);
+        animator.SetBool("Run", playerHasXAxisSpeed);
 
 
     }
@@ -105,11 +138,12 @@ public class PlayerController : MonoBehaviour
         //这个页面也可以看到Jump绑定的按键，可以自行修改
         if (Input.GetButtonDown("Jump"))
         {
+            Debug.Log("跳跃");
             //如果接触地面，才能跳跃
             if (isGround)
             {
-                 //设置跳跃条件
-                animator.SetBool("Jump",true);
+                //设置跳跃条件
+                animator.SetBool("Jump", true);
                 //跳跃只是y轴变化，所以x速度设置为0，y轴设置我们定义的变量，在unity设置初始值
                 Vector2 jumpvel = new Vector2(0.0f, jumpSpeed);
                 //把设置好的参数赋予角色刚体,设置跳跃 ，Vector2.up是Vector2(0, 1)的简单写法
@@ -119,20 +153,69 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
-                   //如果不在地面，就判断是否可以二段跳
+                //如果不在地面，就判断是否可以二段跳
                 if (canDoubleJump)
                 {
                     //设置二段跳跃条件
                     animator.SetBool("DoubleJump", true);
                     //设置二段跳速度
-                    Vector2 doubleJumpVel = new Vector2(0.0f,doubleJumpSpeed);
+                    Vector2 doubleJumpVel = new Vector2(0.0f, doubleJumpSpeed);
                     rigidbody2D.velocity = Vector2.up * doubleJumpVel;
                     canDoubleJump = false;
                 }
             }
 
 
-         }
+        }
+    }
+
+    //爬梯子
+    void Climb()
+    {
+        //是否接触了梯子
+        if (isLadder)
+        {
+            //判断是否按下上和下键
+            //这个方法判断按下下和上和单项平台那个方法差不多，都可以用
+            float moveY = Input.GetAxis("Vertical");
+            if (moveY > 0.5f || moveY < -0.5f)
+            {
+                //播放爬梯子动画
+                animator.SetBool("Climbing", true);
+                //爬梯子的时候，不受重力影响，把重力改成0
+                rigidbody2D.gravityScale = 0.0f;
+                //给个爬梯子的y轴速度
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, moveY*climbSpeed);
+            }
+            //接触了梯子，但是没按上和下键
+            else
+            {
+                //如果在这些途中就算碰到了梯子也不攀爬
+                if (isJumping || isFalling || isDoubleFalling || isDoubleJumping)
+                {
+                    Debug.Log("不攀爬");
+
+                    //不触发动画
+                    animator.SetBool("Climbing", false);
+
+                }
+                //在梯子上，但是并没有持续按住上和下，所以重力不恢复停在梯子上，y轴的速度为0
+                if(isClimbing)
+                {
+                    rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0.0f);
+
+                }
+            }
+        }
+        //如果没接触到梯子
+        else
+        {
+
+            //不触发动画，设为默认值
+            animator.SetBool("Climbing", false);
+            //没碰到时，恢复重力为默认重力
+            rigidbody2D.gravityScale = playerGravity;
+        }
     }
 
     //攻击
@@ -151,32 +234,43 @@ public class PlayerController : MonoBehaviour
     {
         //IsTouchingLayers：判断是否接触定义的图层，谁调用就拿谁去判断，因为这个类是角色类绑定在角色身上，所以是判断角色是否和对应图层接触
         //LayerMask.GetMask("Ground") 根据图层名获取对应图层 Ground就是我们给地面定义的图层，然后给地面选中图层后，如果角色接触到地面就会返回true
-        isGround = myfeet.IsTouchingLayers(LayerMask.GetMask("Ground"))  || myfeet.IsTouchingLayers(LayerMask.GetMask("MovingPlatform"));
+        isGround = myfeet.IsTouchingLayers(LayerMask.GetMask("Ground"))
+            || myfeet.IsTouchingLayers(LayerMask.GetMask("MovingPlatform"))
+            || myfeet.IsTouchingLayers(LayerMask.GetMask("OneWayPlatform"));
 
+        isOneWayPlatform = myfeet.IsTouchingLayers(LayerMask.GetMask("OneWayPlatform"));
 
+    }
+
+    //检测目前接触的是不是梯子
+    void CheckLadder()
+    {
+        //用layer判断是角色脚下的box碰撞体是否接触到梯子
+        isLadder = myfeet.IsTouchingLayers(LayerMask.GetMask("Ladder"));
     }
 
     //切换动画
     void SwitchAnimation()
     {
         //默认idle为false
-        animator.SetBool("Idle",false);
+        animator.SetBool("Idle", false);
         //判断跳跃参数是否为true
         if (animator.GetBool("Jump"))
         {
             //根据y轴的速度判断 上升的话y轴速度是正数，如果下降那么y轴速度会变为负数 所以我们根据速度判断是否正在下落
-            if(rigidbody2D.velocity.y < 0.0f){
+            if (rigidbody2D.velocity.y < 0.0f)
+            {
                 //给下落动画的条件赋值
                 animator.SetBool("Jump", false);
-                animator.SetBool("Fall",true);
+                animator.SetBool("Fall", true);
             }
         }
         //如果接触到地面
         else if (isGround)
         {
             //设置下落转换站立的动画条件
-            animator.SetBool("Fall",false);
-            animator.SetBool("Idle",true);
+            animator.SetBool("Fall", false);
+            animator.SetBool("Idle", true);
         }
 
 
@@ -200,6 +294,51 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("DoubleFall", false);
             animator.SetBool("Idle", true);
         }
+    }
+
+    //判断单向平台向下跳
+    void OneWayPlatfromCheck()
+    {
+        //判断是否在单向平台上
+        //GetAxisRaw:因为Vertical在unity内默认是有上下两个按键的，但是给buttondown只能判断设置了一个按键的，所以这里使用GetAxisRaw，具体可以看笔记
+        if (isOneWayPlatform && Input.GetAxisRaw("Vertical") == -1.0f)
+        {
+            //把玩家的层改成单向平台的层，两个相同的层不会产生碰撞，然后角色就会往下掉
+            gameObject.layer = LayerMask.NameToLayer("OneWayPlatform");
+            //调用掉落动画 这条线其实是从站立直接调用的下落动画，记得在Animator中给Idle设置一条到Fall的线
+            animator.SetBool("Fall", true);
+            Invoke("ResetLayer", resetTime);
+
+        }
+
+    }
+    //重置角色的层
+    void ResetLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Player");
+        //如果落到了地面或者单向平台 回到站立动画
+        if (isGround || isOneWayPlatform)
+        {
+            //返回站立动画
+            animator.SetBool("Fall", false);
+            animator.SetBool("Idle", true);
+        }
+
+
+    }
+
+
+
+    //检测空中的状态
+    void CheckAirStatus()
+    {
+        isJumping = animator.GetBool("Jump");
+        isFalling = animator.GetBool("Fall");
+        isDoubleJumping = animator.GetBool("DoubleJump");
+        isDoubleFalling = animator.GetBool("DoubleFall");
+        isClimbing = animator.GetBool("Climbing");
+
+
     }
 }
 
